1f3f14ef335b03a699d48aca86ee74037a707850
Mahfuz Anan <95133000+MahfuzAnan@users.noreply.github.com>
2023-05-30 00:22:17 +0600

    Revert "After implementing search"

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-
+# CSE4410-Project
+ 
--- dbinfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$user = 'root';
-$pass = '';
-$host = 'localhost';   
-$database = 'library management system';
-date_default_timezone_set('Asia/Dhaka');
-?>
